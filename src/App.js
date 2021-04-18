508d25e935a89335b6c1bd468e0d171609966552
Danaproject <decoratio@gmail.com>
2021-04-18 15:49:42 +0200

    To-do list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
+import { Component } from 'react';
+import TodoList from './components/TodoList';
+import initialTodos from './todos.json';
 import Container from './components/Container/Container';
-import './styles.css';
 
-function App() {
-  return (
-    <Container></Container>
-  );
+class App extends Component {
+  state = {
+    todos: initialTodos,
+  };
+
+  deleteTodo = todoId => {
+    this.setState(prevState => ({
+      todos: prevState.todos.filter(todo => todo.id !== todoId),
+    }));
+  };
+
+  render() {
+    const { todos } = this.state;
+
+    const totalTodoCount = todos.length;
+    const completedTodoCount = todos.reduce(
+      (total, todo) => (todo.completed ? total + 1 : total),
+      0,
+    );
+
+    return (
+      <Container>
+        <h1>To-do list</h1>
+
+        <div>
+          <p>Total amount: {totalTodoCount}</p>
+          <p>Number of Completed: {completedTodoCount}</p>
+        </div>
+
+        <TodoList todos={todos} onDeleteTodo={this.deleteTodo} />
+      </Container>
+    );
+  }
 }
 
 export default App;
